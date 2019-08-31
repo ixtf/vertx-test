21fb8964e61d4acb38417132e52a86af05459adb
jzb <ixtf1984@gmail.com>
2019-08-31 20:22:54 +0800

    upload

--- reactor/src/main/java/org/jzb/test/reactor/BackendVerticle.java
+++ reactor/src/main/java/org/jzb/test/reactor/BackendVerticle.java
@@ -2,8 +2,18 @@ package org.jzb.test.reactor;
 
 import io.reactivex.Single;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
+import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.json.JsonArray;
+import lombok.Cleanup;
+import lombok.SneakyThrows;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.Base64;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -12,7 +22,13 @@ import java.util.concurrent.TimeUnit;
  */
 public class BackendVerticle extends AbstractVerticle {
     @Override
-    public void start(Future<Void> future) throws Exception {
+    public void start() throws Exception {
+        vertx.eventBus().<JsonArray>consumer("mes-auto:report:statisticReport:combines", reply -> {
+            final byte[] bytes = test(reply.body());
+            final DeliveryOptions deliveryOptions = new DeliveryOptions().addHeader("Content-Disposition", "attachment;filename=combines.xlsx");
+            reply.reply(bytes, deliveryOptions);
+        });
+
         vertx.eventBus().<String>consumer("dyeing", reply -> {
             final String body = reply.body();
             final int delay = new Random().nextInt(5);
@@ -28,5 +44,25 @@ public class BackendVerticle extends AbstractVerticle {
         });
     }
 
+    @SneakyThrows
+    private byte[] test(JsonArray jsonArray) {
+        for (Object o : jsonArray) {
+            final byte[] buf;
+            if (o instanceof byte[]) {
+                buf = (byte[]) o;
+            } else {
+                final String s = (String) o;
+                buf = Base64.getDecoder().decode(s);
+            }
+            @Cleanup final ByteArrayInputStream bais = new ByteArrayInputStream(buf);
+            @Cleanup final Workbook wb = WorkbookFactory.create(bais);
+            final Sheet sheet = wb.getSheetAt(0);
+            System.out.println(sheet);
+        }
+        @Cleanup final Workbook wb = WorkbookFactory.create(new File("/home/jzb/C车间7月/C.2019-07-31.xlsx"));
+        @Cleanup final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        wb.write(baos);
+        return baos.toByteArray();
+    }
 
 }
--- reactor/src/main/java/org/jzb/test/reactor/ReactorServer.java
+++ reactor/src/main/java/org/jzb/test/reactor/ReactorServer.java
@@ -2,27 +2,76 @@ package org.jzb.test.reactor;
 
 import com.github.ixtf.japp.core.J;
 import com.github.ixtf.persistence.mongo.Jmongo;
-import com.github.ixtf.persistence.mongo.JmongoOptions;
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoClients;
+import io.reactivex.Flowable;
 import io.vertx.core.*;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.eventbus.Message;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonArray;
+import io.vertx.ext.web.FileUpload;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.CookieHandler;
+import io.vertx.ext.web.handler.ResponseContentTypeHandler;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 import java.util.stream.IntStream;
 
+import static javax.ws.rs.core.MediaType.APPLICATION_OCTET_STREAM;
+
 /**
  * @author jzb 2019-06-08
  */
 public class ReactorServer extends AbstractVerticle {
+    public static final Jmongo jmongo = Jmongo.of(JmongoDev.class);
+
+    private void handle(RoutingContext rc, AsyncResult<Message<String>> ar) {
+        if (ar.succeeded()) {
+            final String body = ar.result().body();
+            if (J.nonBlank(body)) {
+                rc.response().end(body);
+            } else {
+                rc.response().end();
+            }
+        } else {
+            rc.fail(ar.cause());
+        }
+    }
+
     @Override
     public void start(Future<Void> future) throws Exception {
         final Router router = Router.router(vertx);
+        router.route().handler(BodyHandler.create());
+        router.route().handler(ResponseContentTypeHandler.create());
+        router.route().handler(CookieHandler.create());
+
+//        System.out.println(System.getProperty("java.io.tmpdir"));
+//        router.route().handler(BodyHandler.create().setUploadsDirectory(System.getProperty("java.io.tmpdir")));
+
+        router.post("/uploads").produces(APPLICATION_OCTET_STREAM).handler(BodyHandler.create().setUploadsDirectory(System.getProperty("java.io.tmpdir"))).handler(rc -> {
+            final io.vertx.reactivex.core.Vertx vertx = io.vertx.reactivex.core.Vertx.newInstance(this.vertx);
+            vertx.rxExecuteBlocking(f -> Flowable.fromIterable(rc.fileUploads())
+                    .map(FileUpload::uploadedFileName)
+                    .flatMapSingle(vertx.fileSystem()::rxReadFile)
+                    .map(io.vertx.reactivex.core.buffer.Buffer::getBytes)
+                    .toList()
+                    .map(list -> {
+                        final JsonArray array = new JsonArray();
+                        list.stream().forEach(array::add);
+                        return array;
+                    })
+                    .subscribe(f::complete, f::fail), false)
+                    .flatMapSingle(it -> vertx.eventBus().<byte[]>rxSend("mes-auto:report:statisticReport:combines", it))
+                    .subscribe(reply -> {
+                        final HttpServerResponse response = rc.response();
+                        final io.vertx.reactivex.core.MultiMap headers = reply.headers();
+                        headers.entries().forEach(it -> response.putHeader(it.getKey(), it.getValue()));
+                        response.end(Buffer.buffer(reply.body()));
+                    }, rc::fail);
+        });
+
         router.get("/test").handler(rc -> {
             IntStream.rangeClosed(1, 10).mapToObj(it -> "id:" + it).forEach(it -> vertx.eventBus().send("dyeing", it));
             rc.response().end();
@@ -46,38 +95,6 @@ public class ReactorServer extends AbstractVerticle {
                 .requestHandler(router)
                 .listen(8080, ar -> future.handle(ar.mapEmpty()));
     }
-
-    private void handle(RoutingContext rc, AsyncResult<Message<String>> ar) {
-        if (ar.succeeded()) {
-            final String body = ar.result().body();
-            if (J.nonBlank(body)) {
-                rc.response().end(body);
-            } else {
-                rc.response().end();
-            }
-        } else {
-            rc.fail(ar.cause());
-        }
-    }
-
-    public static final Jmongo jmongo = new Jmongo(new JmongoOptions() {
-        private final MongoClient mongoClient = MongoClients.create(
-                MongoClientSettings.builder()
-                        .applyConnectionString(new ConnectionString("mongodb://192.168.0.38"))
-//                        .applyToClusterSettings(builder -> builder.maxWaitQueueSize(1000))
-                        .build()
-        );
-
-        @Override
-        public MongoClient client() {
-            return mongoClient;
-        }
-
-        @Override
-        public String dbName() {
-            return "mes-auto";
-        }
-    });
     public static final JedisPool jedisPool = new JedisPool(new JedisPoolConfig(), "192.168.0.38");
 
     public static void main(String[] args) {
